b1c2b1785db4315ff588c072ded91ed4b307d5d7
oletilwe <oletilwelephalala8@gmail.com>
2023-10-31 07:25:45 -0700

    update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,23 +5,20 @@
 /**
 * check_cycle - checks if a singly linked list has a cycle in it
 * @list: input value
+* Return: always 0
 */
-
-struct listint_s {
-int n;
-struct listint_s *next;
-};
-typedef struct listint_s listint_t;
+int check_cycle(listint_t *list);
+{
 listint_t *slow = list;
-listint_t*fast = list;
+listint_t *fast = list;
 	while (slow && fast && fast->next)
 {
 	slow = slow->next;
 	fast = fast->next->next
-		if (slow == fast) {
+		if (slow == fast)
+		{
 			return (-1);
 		}
 }
 return (0);
 }
-
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,5 +4,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+struct listint_s
+{
+int n;
+struct listint_s *next;
+};
+typedef struct listint_s listint_t;
 int check_cycle(listint_t *list);
 #endif
