f34b7e55bafaa74d9754fae274f21a751fd0ccd8
oletilwe <oletilwelephalala8@gmail.com>
2023-10-31 07:57:34 -0700

    Question 10:

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,18 +7,22 @@
 * @list: input value
 * Return: always 0
 */
+
 int check_cycle(listint_t *list);
 {
-listint_t *slow = list;
-listint_t *fast = list;
-	while (slow && fast && fast->next)
-{
-	slow = slow->next;
-	fast = fast->next->next
-		if (slow == fast)
-		{
-			return (-1);
-		}
-}
-return (0);
+	 listint_t *slow = list;
+        listint_t *fast = list;
+
+        if (!list)
+                return (0);
+
+        while (slow && fast && fast->next)
+        {
+                slow = slow->next;
+                fast = fast->next->next;
+                if (slow == fast)
+                        return (1);
+        }
+
+        return (0);
 }
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,14 +1,15 @@
-#!/usr/bin/python3
-#ifndef LISTS.H
-#define LISTS.H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
-struct listint_s
+typedef struct listint_s
 {
-int n;
-struct listint_s *next;
-};
-typedef struct listint_s listint_t;
+        int n;
+        struct listint_s *next;
+} listint_t;
 int check_cycle(listint_t *list);
+void print_listint(const listint_t *h);
+void free_listint(listint_t *head);
+listint_t *add_nodeint(listint_t **head, int n);
 #endif
