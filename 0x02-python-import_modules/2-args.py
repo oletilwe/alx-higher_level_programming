15c0079bafad4f4690fdbc7bbbb10e7dc56afb9e
oletilwe <oletilwelephalala8@gmail.com>
2023-11-02 10:49:45 -0700

    ...000

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
 if size > 1:
          print("{} arguments:".format(size))
          for i in range(1, size + 1):
-             print("{}: {}".format(i, arg[i]))
+            print("{}: {}".format(i, arg[i]))
 elif size == 0:
-                 print("{} arguments.".format(size))
+        print("{} arguments.".format(size))
 else:
-                     print("{} argument:".format(size))
-                     print("{}: {}".format(size, arg[1]))
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, arg[1]))
