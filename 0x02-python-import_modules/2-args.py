441471eb5f179fc168290a31a85a3045e5b8d359
oletilwe <oletilwelephalala8@gmail.com>
2023-11-02 10:26:05 -0700

    ........

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    num_args = len(argv)
-    if num_args == 0:
-        print(f"Number of arguments: 0.")
-    elif num_args == 1:
-        print(f"Number of arguments: 1.")
-        print(f"1: {argv[0]}")
-    else:
-        print(f"Number of arguments: {num_args}:")
-        for i, arg in enumerate(argv):
-            print(f"{i+1}: {arg}")
+    arg = sys.argv
+    size = len(arg) - 1
+if size > 1:
+         print("{} arguments:".format(size))
+         for i in range(1, size + 1):
+             print("{}: {}".format(i, arg[i]))
+             elif size == 0:
+                 print("{} arguments.".format(size))
+                 else:
+                     print("{} argument:".format(size))
+                     print("{}: {}".format(size, arg[1]))
