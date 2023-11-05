03a3d665885026e4a2a43cf035bb8bf7634937ae
oletilwe <oletilwelephalala8@gmail.com>
2023-11-05 05:09:58 -0800

    .....

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -19,4 +19,14 @@ def delete_at(my_list=[], idx=0):
 int is_palindrome(listint_t **head);
 void print_python_list_info(PyObject *p);
 
+typedef struct listint_s
+{
+  int n;
+  struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
 #endif
