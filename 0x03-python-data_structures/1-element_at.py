3caa94701bd87c48784e725842315cb7f5e36059
oletilwe <oletilwelephalala8@gmail.com>
2023-11-06 00:21:42 -0800

    Q1 -3

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def element_at(my_list, idx):
-        if idx < 0 or idx >= len(my_list):
-            return None
-        return my_list[idx]
+def element_at(my_list, idx):
+    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
