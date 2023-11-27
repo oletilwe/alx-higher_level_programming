7bd58128dc0403a6f2e0a76bad074b93b146ee19
oletilwe <oletilwelephalala8@gmail.com>
2023-11-27 01:56:38 -0800

    main files and Q0 and 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+
+class Rectangle:
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self._width = 0
+        self._height = 0
+        self.width = width
+        self.height = height
+
+    def width(self):
+        return self._width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    def height(self):
+        return self._height
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
