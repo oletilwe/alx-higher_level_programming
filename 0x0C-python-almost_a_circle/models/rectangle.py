7bc244ec08d92052bd19c3a6a646cc95cffdd21f
oletilwe <oletilwelephalala8@gmail.com>
2023-12-08 14:17:06 -0800

    im tired of this shit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,61 +1,177 @@
-#!/usr/bin/python3
-"""Class Rectangle inherits from Base"""
+#!/bin/usr/python3
+"""
+Rectangle module
+"""
 from models.base import Base
 
 
-def __init__(self, width, height, x=0, y=0, id=None):
-    super().__init__(id)
+class Rectangle(Base):
+    """
+    Rectangle class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+        """
+        super().__init__(id)
 
-    self.__width = width
-    self.__height = height
-    self.__x = x
-    self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    
+    def width(self):
+        """
+        width getter
+        """
+        return self.__width
+
+    
+    def width(self, value):
+        """
+        Width setter
+        """
+        # added a check for when value is a bool, if the check is removed
+        # the unittest for it will fail
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    
+    def height(self):
+        """
+        height getter
+        """
+        return self.__height
+
+    
+    def height(self, value):
+        """
+        height setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    
+    def x(self):
+        """
+        x getter
+        """
+        return self.__x
+
+    
+    def x(self, value):
+        """
+        x setter
+        """
+        # added a check for when value is a bool, if the check is removed
+        # the unittest for it will fail
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    
+    def y(self):
+        """
+        y getter
+        """
+        return self.__y
+
+    
+    def y(self, value):
+        """
+        y setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """
+        Returna area of the rectangle
+        """
+        area = self.width * self.height
+
+        return area
+
+    def display(self):
+        """
+        Prints size of rectangle using #
+        """
+        for _ in range(self.y):
+            print()
+
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def __str__(self):
+        """
+        Return the print() and str() representation of the Rectangle.
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x,
+                                                       self.y,
+                                                       self.width,
+                                                       self.height)
+
+    def update(self, *args, **kwargs):
+        """
+        Assign arguments to attributes based on their positions.
+        """
+        if args:
+            for count, arg in enumerate(args):
+                if count == 0:
+                    self.id = arg
+                elif count == 1:
+                    self.width = arg
+                elif count == 2:
+                    self.height = arg
+                elif count == 3:
+                    self.x = arg
+                elif count == 4:
+                    self.y = arg
+                else:
+                    break
+                
+        elif len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+                # removed the break statement, incase if the passed args are greater
+                # than 5, and one of the attributes is at the end
+
+    def to_dictionary(self):
+        """
+        Represents a dictionary representation of rectangle
+        """
+        rec_dict = {
+                "id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+        }
+
+        return rec_dict
 
 
-def width(self):
-    return self.__width
-
-
-def width(self, value):
-    self._validate_positive_integer("width", value)
-    self.__width = value
-
-
-def height(self):
-    return self.__height
-
-
-def height(self, value):
-    self._validate_positive_integer("height", value)
-    self.__height = value
-
-
-def x(self):
-    return self.__x
-
-
-def x(self, value):
-    self._validate_non_negative_integer("x", value)
-    self.__x = value
-
-
-def y(self):
-    return self.__y
-
-
-def y(self, value):
-    self._validate_non_negative_integer("y", value)
-    self.__y = value
-
-
-def _validate_positive_integer(self, attribute_name, value):
-    if not isinstance(value, int):
-        raise TypeError(f"{attribute_name} must be an integer.")
-    elif value <= 0:
-        raise ValueError(f"{attribute_name} must be > 0.")
-
-def _validate_non_negative_integer(self, attribute_name, value):
-    if not isinstance(value, int):
-        raise TypeError(f"{attribute_name} must be an integer.")
-    elif value < 0:
-        raise ValueError(f"{attribute_name} must be >= 0.")
+if __name__ == "__main__":
+    pass
---
+++ 0x0C-python-almost_a_circle/tests/README.md
@@ -0,0 +1,2 @@
+tests for:
+0x0C. Python - Almost a circle
