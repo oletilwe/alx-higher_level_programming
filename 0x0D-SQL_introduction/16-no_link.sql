e3f0774011653c99313be2ca851b3ea9a5d27043
oletilwe <oletilwelephalala8@gmail.com>
2024-01-16 13:47:04 -0800

    third commit

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- a script that lists all records of the table second_table of the database hbtn_0c_0
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
