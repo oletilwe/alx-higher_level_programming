bdb86c83f65533b4cb01382e71ed21f5d2651545
oletilwe <oletilwelephalala8@gmail.com>
2024-01-17 12:41:34 -0800

    updates Q13 - 16

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server:
-SELECT
-    tv_show_genres.genre_id AS genre,
-    COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM
-    tv_show_genres
-GROUP BY
-    tv_show_genres.genre_id
-ORDER BY
-    number_of_shows DESC;
+-- lists all rows of a database meeting a condition
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,12 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL
-SELECT
-    tv_shows.title,
-    tv_genres.name AS genre
-FROM
-    tv_shows
-LEFT JOIN
-    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN
-    tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY
-    tv_shows.title ASC, tv_genres.name ASC;
+-- lists all rows of a table linked to another table
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
