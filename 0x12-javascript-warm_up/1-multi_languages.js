27c032755ebddcc8db9c5b4286b968a24b9bf915
oletilwe <oletilwelephalala8@gmail.com>
2024-02-12 07:23:50 -0800

    pushing first 3 files

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const firstLine = "C is fun";
+let secondLine = "Python is cool";
+const thirdLine = "JavaScript is amazing";
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
