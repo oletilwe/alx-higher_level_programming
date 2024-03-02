49297f8e86ff3356fb8c1b9986d815ff2324e03c
oletilwe <oletilwelephalala8@gmail.com>
2024-03-01 19:27:41 -0800

    ,,

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,21 @@ import urllib.request
 url = 'https://alx-intranet.hbtn.io/status'
 try:
     with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-        print('-' * 50)
-        print(f"Body response:\n{body}")
-        print('-' * 50)
+        if response.getcode() == 200:
+            body = response.read().decode('utf-8')
+            print('-' * 50)
+            print(f"Body response:\n{body}")
+            print('-' * 50)
+
+            headers = response.info()
+            print("HTTP Headers:")
+            for header, value in headers.items():
+                print(f"{header}: {value}")
+
+        else:
+            print(f"Error code: {response.getcode()}")
+
+except urllib.error.HTTPError as e:
+    print(f"HTTPError: {e}")
 except urllib.error.URLError as e:
-    print(f"Error accessing the URL: {e}")
+    print(f"URLError: {e}")
